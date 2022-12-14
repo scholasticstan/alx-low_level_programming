2d50bb0d5285fe28ad2b97b2e7cc3465dba9a121
Stanley Alu <alu.uzorka.stanley@gmail.com>
2022-12-13 16:47:20 -0800

    corrected

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,17 @@
-int putchar (int character)
+#include <stdio.h>
+
+ int main(void)
+ {
+   char str[1000];
+   int ch, i, n = 0;
+  
+   while ((ch = getchar()) != EOF && n < 1000)
+     str[n++] = ch;
+	   
+   for (i = 0; i < n; ++i)
+     putchar(str[i]);
+
+   putchar('\n'); /* trailing '\n' needed in Standard C */
+	
+   return 0;
+ }
