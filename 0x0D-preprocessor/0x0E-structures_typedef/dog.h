71667d9aba6e3bfcd88c44d168be65b0fb4c9f5d
Stanley Alu <alu.uzorka.stanley@gmail.com>
2023-01-16 22:57:27 -0600

    header file

--- 0x0D-preprocessor/0x0E-structures_typedef/dog.h
+++ 0x0D-preprocessor/0x0E-structures_typedef/dog.h
@@ -1,14 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
- * struct dog - sturct DOG card
- * @name: name of the dog
- * @age: inger represents the age
- * @owner: the name of the owner
- */
-
+ * struct dog - dog structure
+ *
+ * @name: name of dog
+ * @age: age of the dog
+ * @owner: dog owner
+ *
+*/
 struct dog
 {
 	char *name;
@@ -17,8 +17,8 @@ struct dog
 };
 
 /**
- * dog_t - a new type of struct dog
- */
+ * dog_t -Typedef for struct dog
+*/
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -26,4 +26,4 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif
+#endif /* struct dog */
